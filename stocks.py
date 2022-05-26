de13e8c2be9f0b47160388bec79a6d0cff57dfff
Rikesh-Patel <103740388+Rikesh-Patel@users.noreply.github.com>
2022-05-26 16:55:30 -0700

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -457,7 +457,7 @@ if st.button('Predict'):
 	     To note: The Long Short-Term Memory neural network an ongoing list of both pros and cons. For instance, these neural networks can be fine
 	     tuned along parameters, but this is time-intensive and hyper-focused. In terms of efficiency, we can conclude that the LSTM network's
 	     performance fails to meet expectations when compared to other relevant models, like AR and ARIMA that are better fits for modeling stock price
-	     time series data. LSTM is inferior in terms of extrapolating ability and requiring large and consistent data."""
+	     time series data. LSTM is inferior in terms of extrapolating ability and requiring large and consistent data.""")
 
 st.markdown("""* **Data source :** [Stock Market MBA](https://stockmarketmba.com/stocksinthesp500.php), [Polygon API](https://api.polygon.io/), [ETF Database](https://etfdb.com/compare/market-cap/)
 """)
